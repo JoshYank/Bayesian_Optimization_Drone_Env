890f821d40c38157b196edc29a7f1cac307f141f
JoshYank <89559188+JoshYank@users.noreply.github.com>
2023-04-10 11:34:44 -0400

    Update BO_CMAES_Coding_1_0.py

--- BO_CMAES/BO_CMAES_Coding_1_0.py
+++ BO_CMAES/BO_CMAES_Coding_1_0.py
@@ -16,22 +16,59 @@ class BO_CMAES:
     def __init__(
             self,
             spec,
-            boundary,
+            boundary,       #bounds=np.array([[-10,10],[-10,10]])
             budget,
             population_size):
         self.spec=spec              #Robustness function with built in simulation
-        self.global_bounds=boundary     #global boundaries of variables [(),(),..]
+        self.global_bounds=boundary     #global boundaries of variables
         self.population_size=population_size #number of iterations per localized model
+        self.budget=budget
         
         
     def initialize(self):
+        #initiate CMAES to calculate variance
+        Sim_count=0
+        self.CMA_optimizer=CMA(mean=np.zeros(len(self.global_bounds)),sigma=1.3,
+                          bounds=self.global_bounds,
+                          population_size=(self.population_size))
+        self.CMA_optimizer._mu=self.population_size//4       #set number of best ind for eval
+        #initialize fist sampling
         format_glob_bound=set_bounds(self.global_bounds)
         self.global_opt=BayesianOptimization(f=self.spec,domain=format_glob_bound,
                                         acquisition_type='LCB',
                                         initial_design_numdata=self.population_size)
-        best_ind_loc=[]
-        for i in range(5):
-            best_ind_loc.append(self.global_opt.Y.T.argsort()[0][i])
+        Sim_count+=len(self.global_opt.X)
+        solutions=[]
+        for _ in range(self.population_size):
+            solutions.append((self.global_opt.X[_],self.global_opt.Y[_]))
+        self.CMA_optimizer.tell(solutions)
+        self.Global_Min_Robust=self.global_opt.Y.min()
+        
+        sigma=self.CMA_optimizer._sigma
+        mean=self.CMA_optimizer._mean
+        variance=[]
+        for i in range(len(self.CMA_optimizer._C)):
+            variance.append(self.CMA_optimizer._C[i][i])
+        dev=np.sqrt(variance)
+        self.local_bounds=[]
+        for i in range(len(self.global_bounds)):
+            lower_bounds=mean[i]-sigma*dev[i]
+            upper_bounds=mean[i]+sigma*dev[i]
+            self.local_bounds.append([lower_bounds,upper_bounds])
+    """
+        while Sim_count<self.budget:
+            sigma=self.CMA_optimizer._sigma
+            mean=self.CMA_optimizer._mean
+            variance=[]
+            for i in range(len(self.CMA_optimizer._C)):
+                variance.append(self.CMA_optimizer._C[i][i])
+            dev=np.sqrt(variance)
+            self.local_bounds=[]
+            for i in range(len(self.global_bounds)):
+                lower_bounds=mean[i]-sigma*dev[i]
+                upper_bounds=mean[i]+sigma*dev[i]
+                self.local_bounds.append([lower_bounds,upper_bounds])
+        """     
         
 
 #function to define boundaries (both global and local)
