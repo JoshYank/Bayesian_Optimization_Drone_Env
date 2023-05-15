6479678619c116465e08055be8a48769bc9f674a
JoshYank <89559188+JoshYank@users.noreply.github.com>
2023-05-15 13:09:46 -0400

    Update BO_CMA_AT.py

--- BO_CMAES/BO_CMA_AT.py
+++ BO_CMAES/BO_CMA_AT.py
@@ -23,8 +23,8 @@ import logging
 
 from GPyOpt.methods import BayesianOptimization
 
-from BO_CMAES_Coding_1_0 import BO_CMAES
-from BO_CMAES_Coding_1_0 import set_bounds
+from BO_CMAES_Coding_1_2 import BO_CMAES
+from BO_CMAES_Coding_1_2 import set_bounds
 
 eng = matlab.engine.start_matlab() #connect matlab
 def compute_traj(**kwargs):
@@ -76,14 +76,42 @@ def pred2(x):
     return np.array(Y)
 
 bounds=np.array([[0.,100.],[0.,100.],[0.,100.],[0.,100.]])
+B=set_bounds(bounds)
+
+rand_num=[16245,18762,1929,921,2817];
+#rand_num=[21,2817]
+BO_Robust=[]
+BO_Counter=[]
+BCMA_Robb=[]
+BCMA_Counter=[]
+BCMA_Jump=[]
+for r in rand_num:
+    np.random.seed(r)
+    
+    Test=BO_CMAES(spec=pred2,boundary=bounds,budget=120,population_size=20)
+    Test.initialize()
+    Test.run_BO_CMA()
+    Test.get_violation_count()
+    BCMA_Counter.append(Test.Violation_Count)
+    BCMA_Robb.append(Test.Global_Min_Robust)
+    BCMA_Jump.append(Test.Record_jump)
+    
+    BO_Test=BayesianOptimization(f=pred2,domain=B, acquisition_type='LCB',initial_design_numdata=20)
+    BO_Test.run_optimization(max_iter=120)
+    BO_Counter.append(np.sum(BO_Test.Y<0))
+    BO_Robust.append(BO_Test.Y.min())
+    
+"""
 #---------------------------------------------------------------------------------
 #BO-CMAES
-Test=BO_CMAES(spec=pred1,boundary=bounds,budget=220,population_size=20)
+Test=BO_CMAES(spec=pred2,boundary=bounds,budget=440,population_size=40)
 Test.initialize()
 Test.run_BO_CMA()
 
 #-------------------------------------------------------------------------------
 #BO
+
 B=set_bounds(bounds)
 BO_Test=BayesianOptimization(f=pred1,domain=B, acquisition_type='LCB',initial_design_numdata=20)
 BO_Test.run_optimization(max_iter=200)
+"""
